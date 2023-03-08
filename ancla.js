7aa5267fffcb94775c2ae90de459e3d1e4f08535
Rodriguez05-dav <126629934+Rodriguez05-dav@users.noreply.github.com>
2023-03-08 20:41:53 -0300

    initial commit

---
+++ ancla.js
@@ -0,0 +1,13 @@
+
+document.getElementById('personal').onclick = function () {
+    document.location.href = "datospersonales.html"
+}
+document.getElementById('estudios').onclick = function () {
+    document.location.href = "estudios.html"
+}
+document.getElementById('experiencia').onclick = function () {
+    document.location.href = "experiencia.html"
+}
+document.getElementById('extras').onclick = function () {
+    document.location.href = "extras.html"
+}
